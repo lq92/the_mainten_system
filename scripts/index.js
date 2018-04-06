e3bea7976c677c945d2908c531c4ba0ff05791a1
lq92 <15157474420@163.com>
2018-04-06 09:03:11 +0800

    optimize the javascript code and recompress the styles sheet

--- scripts/index.js
+++ scripts/index.js
@@ -5,17 +5,17 @@ function hide(obj){
 	obj.style.display = 'none'
 }
 function random(min, max){
-	return Math.floor((max - min + 1) * Math.random()) + min;
+	return Math.floor((max - min + 1) * Math.random()) + min;  // 返回一个随机整数
 }
 function createElement(ele, cssObj){
 	ele = document.createElement(ele);
 	ele.style.cssText = cssObj;
 	return ele;
 }
-function Area(position, name = '1号小区' , statusColor = 'rgb(71, 209, 71)'){
-	this.position = position || { x: random(0, 1000), y: random(0, 450), width: 24, height: 24};
-	this.name = name;
-	this.statusColor = statusColor;
+function Area(info){
+	this.position = info.position || { x: random(0, 1000), y: random(0, 450), width: 24, height: 24};
+	this.name = info.name || '1号小区';
+	this.statusColor = info.statusColor || 'rgb(71, 209, 71)';
 }
 Area.prototype.init = function(){
 	let divObj = `
@@ -67,7 +67,7 @@ window.addEventListener('load', () => {
 			mapInfoHook = document.querySelector('.map_info_hook'),
 			threedInfoHook = document.querySelector('.threed_info_hook'),
 			mapBoxHook = document.querySelector('.map_box_hook'),
-			threedBoxHook = document.querySelector('.threed_box_hook')
+			threedBoxHook = document.querySelector('.threed_box_hook');
 	map.addEventListener('click', () => {
 		show(mapInfoHook)
 		hide(threedInfoHook)
@@ -84,46 +84,19 @@ window.addEventListener('load', () => {
 		map.classList.toggle('active')
 		threeD.classList.toggle('active')
 	}, false)	
-	// 1700 906
-	// fault: rgb(245, 64, 64), normal: rgb(71, 209, 71), mainten: rgb(255, 241, 84)
-	let onePostion = { x: '16%', y: '36%', width: 24, height: 24},
-			oneName = '3号小区',
-			oneStatusColor = 'rgb(245, 64, 64)',
-			one = new Area(onePostion, oneName, oneStatusColor);
-	let twoPosition = { x: '4%', y: '73%', width: 24, height: 24},
-			twoName = '2号小区',
-			twoStatusColor = 'rgb(255, 241, 84)',
-			two = new Area(twoPosition, twoName, twoStatusColor);
-	let threePosition = { x: '39%', y: '19%', width: 24, height: 24},
-			threeName = '3号小区',
-			threeStatusColor = 'rgb(245, 64, 64)',
-			three = new Area(threePosition, threeName, threeStatusColor);
-	let fourPosition = { x: '37%', y: '86%', width: 24, height: 24},
-			fourName = '2号小区',
-			fourStatusColor = 'rgb(255, 241, 84)',
-			four = new Area(fourPosition, fourName, fourStatusColor);
-	let fivePosition = { x: '49%', y: '50%', width: 24, height: 24},
-			fiveName = '3号小区',
-			fiveStatusColor = 'rgb(245, 64, 64)',
-			five = new Area(fivePosition, fiveName, fiveStatusColor);
-	let sixPosition = { x: '63%', y: '58%', width: 24, height: 24},
-			sixName = '4号小区',
-			sixStatusColor = 'rgb(71, 209, 71)',
-			six = new Area(sixPosition, sixName, sixStatusColor);
-	let sevenPosition = { x: '76%', y: '34%', width: 24, height: 24},
-			sevenName = '4号小区',
-			sevenStatusColor = 'rgb(71, 209, 71)',
-			seven = new Area(sevenPosition, sevenName, sevenStatusColor);
-	let eightPosition = { x: '86%', y: '75%', width: 24, height: 24},
-			eightName = '4号小区',
-			eightStatusColor = 'rgb(71, 209, 71)',
-			eight = new Area(eightPosition, eightName, eightStatusColor);
-	mapBoxHook.appendChild(one.init())
-	mapBoxHook.appendChild(two.init())
-	mapBoxHook.appendChild(three.init())
-	mapBoxHook.appendChild(four.init())
-	mapBoxHook.appendChild(five.init())
-	mapBoxHook.appendChild(six.init())
-	mapBoxHook.appendChild(seven.init())
-	mapBoxHook.appendChild(eight.init())
+	let positionArr = [
+		{ position: { x: '16%', y: '36%', width: 24, height: 24}, name: '3号小区', statusColor: 'rgb(245, 64, 64)' },
+		{ position: { x: '4%', y: '73%', width: 24, height: 24}, name: '2号小区', statusColor: 'rgb(255, 241, 84)' },
+		{ position: { x: '39%', y: '19%', width: 24, height: 24}, name: '3号小区', statusColor: 'rgb(245, 64, 64)' },
+		{ position: { x: '37%', y: '86%', width: 24, height: 24}, name: '2号小区', statusColor: 'rgb(245, 64, 64)' },
+		{ position: { x: '49%', y: '50%', width: 24, height: 24}, name: '3号小区', statusColor: 'rgb(245, 64, 64)' },
+		{ position: { x: '63%', y: '58%', width: 24, height: 24}, name: '4号小区', statusColor: 'rgb(71, 209, 71)' },
+		{ position: { x: '76%', y: '34%', width: 24, height: 24}, name: '4号小区', statusColor: 'rgb(71, 209, 71)' },
+		{ position: { x: '86%', y: '75%', width: 24, height: 24}, name: '4号小区', statusColor: 'rgb(71, 209, 71)' }
+	];
+	let documentfragment = document.createDocumentFragment();
+	positionArr.forEach(item => {
+		documentfragment.appendChild(new Area(item).init())
+	})
+	mapBoxHook.appendChild(documentfragment)
 }, false)
